611b7f104600bf3b0f846f6262b25652a6485d19
cosmoTechnologist <cosmoTechnologist@yahoo.com>
2017-01-04 01:40:24 +0800

    Revert "login and signin"

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,8 @@ Route::get('/', function (){
 });
 
 Route::group(['middleware' => ['web']], function () {
-
     Route::post('/signup', [
         'uses' => 'UserController@postSignUp',
         'as' => 'signup'
-    ]);
-
-    Route::post('/signin', [
-        'uses' => 'UserController@postSignIn',
-        'as' => 'signin'
-    ]);
-
-    Route::get('/dashboard', [
-        'uses' => 'UserController@getDashboard',
-        'as' => 'dashboard'
     ]);
 });
